08f8e01a337aaae81ccbf595468dab814326269c
Ajay-404 <ajaysp22@gmail.com>
2021-08-12 21:07:49 -0400

    Changes Aug 12

--- src/TestJenkins.java
+++ src/TestJenkins.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class TestJenkins {
     public static void main(String[]args)
     {
+
         Random rand = new Random(); //instance of random class
         int num = 0;
         int upperbound = 25;
